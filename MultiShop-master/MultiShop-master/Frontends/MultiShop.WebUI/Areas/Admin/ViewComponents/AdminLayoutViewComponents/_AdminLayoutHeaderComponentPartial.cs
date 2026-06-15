30083eeb6ca6c87baac9bc9837115bf873a6ab7e
Eren Yurtaslan <abdulkadir.yurtaslan@ogr.sakarya.edu.tr>
2026-06-15 23:47:41 +0300

    added _AdminLayoutHeaderComponentPartial.cs

---
+++ MultiShop-master/MultiShop-master/Frontends/MultiShop.WebUI/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminLayoutHeaderComponentPartial.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Services.CommentServices;
+using MultiShop.WebUI.Services.Interfaces;
+using MultiShop.WebUI.Services.MessageServices;
+
+namespace MultiShop.WebUI.Areas.Admin.ViewComponents.AdminLayoutViewComponents
+{
+    public class _AdminLayoutHeaderComponentPartial : ViewComponent
+    {
+        private readonly IMessageService _messageService;
+        private readonly IUserService _userService;
+        private readonly ICommentService _commentService;
+        public _AdminLayoutHeaderComponentPartial(IMessageService messageService, IUserService userService, ICommentService commentService)
+        {
+            _messageService = messageService;
+            _userService = userService;
+            _commentService = commentService;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userService.GetUserInfo();
+            var userId = user?.Id ?? string.Empty;
+            int messageCount = 0;
+            if (!string.IsNullOrWhiteSpace(userId))
+            {
+                messageCount = await _messageService.GetTotalMessageCountByReceiverId(userId);
+            }
+            ViewBag.messageCount = messageCount;
+
+            int totalCommentcount = await _commentService.GetTotalCommentCount();
+            ViewBag.totalCommentCount = totalCommentcount;  
+
+            return View();
+        }
+    }
+}
