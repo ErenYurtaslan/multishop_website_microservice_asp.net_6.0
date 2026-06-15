def54df4a5276af59f5dde46c91c71a55ba12bae
Eren Yurtaslan <abdulkadir.yurtaslan@ogr.sakarya.edu.tr>
2026-06-16 00:05:28 +0300

    added dto files

---
+++ MultiShop-master/MultiShop-master/Services/Catalog/MultiShop.Catalog/Dtos/ProductDtos/ProductFilterRequestDto.cs
@@ -0,0 +1,13 @@
+namespace MultiShop.Catalog.Dtos.ProductDtos
+{
+    public class ProductFilterRequestDto
+    {
+        public string? CategoryId { get; set; }
+        public string? Color { get; set; }
+        public string? Size { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+        public bool InStockOnly { get; set; }
+        public string? Search { get; set; }
+    }
+}
